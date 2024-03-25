22ca57b4cf81b536429c87c10f5df41352bb0b5b
Alethea Kramer <aletheakramer@outlook.com>
2024-03-25 11:26:38 -0700

    added data visualizations

--- my-react-app/src/components/Block.js
+++ my-react-app/src/components/Block.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import CustomBarChart from './BarChart'
+import RentalTypeDensityPieChart from './PieChart'
+import VacancyCard from './VacancyCard'
+// import BarChartMockup from './BarChart'
+
 
 
 const Block = () => {
     return (
         <div>
-
+            <CustomBarChart></CustomBarChart>
+            <RentalTypeDensityPieChart></RentalTypeDensityPieChart>
+            <VacancyCard></VacancyCard>
         </div>
     )
 }
