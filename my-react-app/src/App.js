1daee9b9e234fc887cab68ae5c3d7faf274098fe
ivyhan1 <ivyhan1@students.cs.ubc.ca>
2024-04-15 15:48:17 -0700

    created Google log-in button

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import FiltersForm from './components/FiltersForm.js';
 import Block from './components/Block.js';
 import logoSvg from './LeaseLyticsLogoBlue.svg';
+import { useEffect } from 'react';
 
 function App() {
   const [request, setRequest] = useState({});
   const [response, setResponse] = useState('');
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+  
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "1000336939480-fgfku4o35kcj9n7tadb47ujdi5q3t5c9.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    })
+
+    google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline", size: "large"}
+    );
+  }, []);
 
   const handleResponse = (req, newResponse) => {
     console.log("App req:")
@@ -19,6 +37,7 @@ function App() {
 
   return (
     <div className="App">
+      <div id="signInDiv"></div>
       <div className="container" style={{ width: '100%' }}>
         <div className="form">
           <img src={logoSvg} alt="Logo" style={{ width: '400px', height: 'auto' }} />
