f372f8243f8989c8aef7f057e1c50114408bb4f6
ivyhan1 <ivyhan1@students.cs.ubc.ca>
2024-04-15 16:27:17 -0700

    email prompt side bar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -35,6 +35,8 @@ function App() {
         document.getElementById("signInDiv"),
         { theme: "outline", size: "large"}
     );
+
+    google.accounts.id.prompt();
   }, []);
 
   // if we have no user: show sign-in button
