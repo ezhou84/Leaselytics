e911db9fc4bb733cd93292069a951fb9a6a8ec8a
Alethea Kramer <aletheakramer@outlook.com>
2024-04-19 15:44:15 -0700

    removed old login

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,6 +6,7 @@ import logoSvg from './LeaseLyticsLogoBlue.svg';
 import { jwtDecode } from "jwt-decode";
 import { createTheme, ThemeProvider } from '@mui/material';
 import LandingPage from './components/Landing.js';
+import Login from './components/Login.js';
 
 const theme = createTheme({
   typography: {
@@ -16,39 +17,7 @@ const theme = createTheme({
 function App() {
   const [request, setRequest] = useState({});
   const [response, setResponse] = useState({});
-  const [user, setUser] = useState({});
 
-  function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    var userObject = jwtDecode(response.credential);
-    console.log(userObject);
-    setUser(userObject);
-    document.getElementById("signInDiv").hidden = true;
-  }
-
-  function handleSignOut(event) {
-    setUser({});
-    // show option to sign-in button
-    document.getElementById("signInDiv").hidden = false;
-  }
-
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "1000336939480-fgfku4o35kcj9n7tadb47ujdi5q3t5c9.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    })
-
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      { theme: "outline", size: "large" }
-    );
-
-    google.accounts.id.prompt();
-  }, []);
-
-  // if we have no user: show sign-in button
-  // if we have a user: show log-out button
 
   const handleResponse = (req, newResponse) => {
     setRequest(req);
@@ -59,16 +28,7 @@ function App() {
     <ThemeProvider theme={theme}>
       {/* <LandingPage></LandingPage> */}
       <div className="App" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <div id="signInDiv"></div>
-        {Object.keys(user).length != 0 &&
-          <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
-        }
-        {user &&
-          <div>
-            <img src={user.picture}></img>
-            <h3>{user.name}</h3>
-          </div>
-        }
+        <Login></Login>
         <div className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', width: '80%' }}>
           <div className="form">
             <img src={logoSvg} alt="Logo" style={{ width: '60%', height: 'auto' }} />
