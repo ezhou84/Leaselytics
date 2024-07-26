5504a030062fdb6b711135c1e8ccce69e116b4f2
Eric Zhou <zhoueric84@gmail.com>
2024-07-25 21:19:28 -0700

    Add cors

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,15 @@ import apiRouter from "./api-router.js";
 
 import { HOST, PORT } from "../../frontend/src/config.js";
 
+import cors from "cors";
+
 const server = express();
+
+server.use(cors());
+
+server.use(cors({
+  origin: 'https://leaselytics.vercel.app/'
+}));
 
 server.use("/api", apiRouter);
 
