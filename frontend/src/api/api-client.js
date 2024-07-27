d6a5b4cc8df01c2da51b23b5349973ee6e3f513a
Eric Zhou <zhoueric84@gmail.com>
2024-07-26 17:55:10 -0700

    Log values

--- frontend/src/api/api-client.js
+++ frontend/src/api/api-client.js
@@ -3,8 +3,11 @@ import axios from "axios";
 import { API_SERVER_URL } from "../config.js";
 
 export const fetchPrice = async (params) => {
+    console.log(API_SERVER_URL);
+    const url = `${API_SERVER_URL}/price`;
+    console.log(url);
     const res = await axios.get(
-        `${API_SERVER_URL}/price`,
+        url,
         params
     );
 
